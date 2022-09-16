db12aa9f3e6aed60c09b041b1f12f8f6963276b3
Shannon Yazdani <shannonyazdani@gmail.com>
2022-09-16 15:25:26 -0400

    updated base url for axios

--- src/utils/api-urls.tsx
+++ src/utils/api-urls.tsx
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:8080";
+export const baseUrl = "https://entertainment-web-app-server.herokuapp.com/";
 export const signUpUrl = `${baseUrl}/users/signup`;
 export const loginUrl = `${baseUrl}/users/login`;
 export const profileUrl = `${baseUrl}/profile`;
