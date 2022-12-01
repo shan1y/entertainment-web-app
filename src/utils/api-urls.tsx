df5d92154c4a505f68e9df889ba7fc9f09f201ad
Shannon Yazdani <shannonyazdani@gmail.com>
2022-11-30 22:09:15 -0400

    change to fly server

--- src/utils/api-urls.tsx
+++ src/utils/api-urls.tsx
@@ -1,4 +1,4 @@
-export const baseUrl = `https://mysql-entertainment-deploy.herokuapp.com`;
+export const baseUrl = `https://bestflix-server.fly.dev`;   
 export const signUpUrl = `${baseUrl}/users/signup`;
 export const loginUrl = `${baseUrl}/users/login`;
 export const profileUrl = `${baseUrl}/profile`;
